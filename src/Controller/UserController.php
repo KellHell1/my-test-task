49144c753d9f648836bc7531adea181cd1b5b93f
Nikolay <mykolagalanix@gmail.com>
2023-07-29 16:45:11 +0300

    update UserController, UserHandler

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,40 +2,61 @@
 
 namespace App\Controller;
 
+use App\Service\UserHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route(path: '/user/info/{user_id}', requirements: ['user_id' => '\d+'], methods: ['GET'])]
-    public function getUserInfoById(int $user_id): void
+    public function __construct(private UserHandler $userHandler)
     {
+    }
+
+
+    #[Route(path: '/user/info/{user_id}', requirements: ['user_id' => '\d+'], methods: ['GET'])]
+    public function getUserInfoById(int $user_id): JsonResponse
+    {
+        $userInfo = $this->userHandler->getUserInfo($user_id);
+
+        return new JsonResponse($userInfo);
     }
 
 
     #[Route(path: '/user/number/balance/increase', methods: ['PATCH'])]
-    public function increaseUserPhoneBalance(Request $request): void
+    public function increaseUserPhoneBalance(Request $request): JsonResponse
     {
+        $this->userHandler->increaseNumberBalance(json_decode($request->getContent(), true));
+
+        return new JsonResponse(['Your balance has been successfully topped up']);
     }
 
 
     #[Route(path: '/user/add', methods: ['POST'])]
-    public function addNewUser(Request $request): void
+    public function addNewUser(Request $request): JsonResponse
     {
+        $this->userHandler->addUser(json_decode($request->getContent(), true));
+
+        return new JsonResponse(['User has been add successfully']);
     }
 
 
     #[Route(path: '/user/number/add', methods: ['POST'])]
-    public function addPhoneNumberForUser(Request $request): void
+    public function addPhoneNumberForUser(Request $request): JsonResponse
     {
+        $this->userHandler->addPhoneNumber(json_decode($request->getContent(), true));
+
+        return new JsonResponse(['Number has been add successfully']);
     }
 
 
     #[Route(path: '/user/info/{user_id}', requirements: ['user_id' => '\d+'], methods: ['DELETE'])]
-    public function deleteAllUserInformation(int $user_id): void
+    public function deleteAllUserInformation(int $user_id): JsonResponse
     {
+        $this->userHandler->deleteUser($user_id);
+
+        return new JsonResponse(['All information about user has been delete successfully']);
     }
 
 }
--- src/Service/UserHandler.php
+++ src/Service/UserHandler.php
@@ -4,45 +4,114 @@ namespace App\Service;
 
 use App\Entity\MobilePhone;
 use App\Entity\User;
+use App\Repository\MobilePhoneRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserHandler
 {
-    private EntityManagerInterface $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserRepository         $userRepository,
+        private MobilePhoneRepository  $mobilePhoneRepository,
+    )
     {
     }
 
-    public function getUserInfo(int $user_id): void
+
+    public function getUserInfo(int $user_id): array
     {
+        $user = $this->userRepository->find($user_id)
+            ?? throw new NotFoundHttpException('user not found');
+
+        $userPhoneNumbers = [];
+
+        foreach ($user->getPhoneNumbers() as $phoneNumber) {
+            $userPhoneNumbers[] = $phoneNumber->getNumber();
+        }
+
+        return [
+            'Name' => $user->getName(),
+            'Year of birth' => $user->getDateBirth()->format('Y'),
+            'Phone numbers' => $userPhoneNumbers
+        ];
     }
 
-    public function increaseNumberBalance(string $number, float $sum): void
+    public function increaseNumberBalance(array $data): void
     {
+        $sum = $data['sum'];
+
+        if ($sum > 100.00) {
+            throw new BadRequestHttpException('Maximum deposit amount per transaction 100.00');
+        }
+
+        $number = $this->mobilePhoneRepository->findOneBy(['number' => $data['number']])
+            ?? throw new NotFoundHttpException('number not found');
+
+        $newNumberBalance = $number->getBalance() + $sum;
+
+        $number->setBalance($newNumberBalance);
+
+        $this->entityManager->flush();
     }
 
-    public function addUser(string $name, string $birthDate, array $numbers): void
+    public function addUser(array $data): void
     {
+        $newUser = new User();
+        $newUser->setName($data['name']);
+        $newUser->setDateBirth(new \DateTime($data['birthDate']));
+
+        $this->entityManager->persist($newUser);
+
+        foreach ($data['numbers'] as $number) {
+            $newMobilePhone = new MobilePhone();
+            $newMobilePhone->setUser($newUser);
+            $newMobilePhone->setNumber($number['number']);
+            $newMobilePhone->setBalance($number['balance']);
+
+            $this->entityManager->persist($newMobilePhone);
+        }
+
+        $this->entityManager->flush();
     }
 
-    public function addPhoneNumber(int $userId, string $number, float $balance): void
+    public function addPhoneNumber(array $data): void
     {
+        $user = $this->userRepository->find($data['userId'])
+            ?? throw new NotFoundHttpException('user not found');
+
+        if (!$this->mobilePhoneRepository->findOneBy(['user' => $user, 'number' => $data['number']])) {
+            $newMobilePhone = new MobilePhone();
+            $newMobilePhone->setUser($user);
+            $newMobilePhone->setNumber($data['number']);
+            $newMobilePhone->setBalance($data['balance']);
+
+            $this->entityManager->persist($newMobilePhone);
+        } else {
+            $this->mobilePhoneRepository->findOneBy(
+                ['user' => $user, 'number' => $data['number']])->setBalance($data['balance']);
+        }
+
+
+        $this->entityManager->flush();
     }
 
     public function deleteUser(int $userId): void
     {
+        $user = $this->userRepository->find($userId)
+            ?? throw new NotFoundHttpException('user not found');
+
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
     }
 
-    public function numberPhoneValidation(string $number): void
+    public function checkEmptyDB()
     {
     }
 
-    public function checkEmptyDB(): void
-    {
-    }
-
-    public function fillingUsersForDB(): void
+    public function fillingUsersForDB()
     {
     }
 }
